662c2a0e66c52f79b50ed4da2bbcf54942b10a0a
Marlon Mendes <marlonciriatico@gmail.com>
2018-10-18 21:06:54 -0300

    Create List

--- main.c
+++ main.c
@@ -1,13 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void openFile(FILE *file);
+void openFile(FILE *);
+
+typedef struct Contact{
+    char name[101];
+    char cell[11];
+    char address[101];
+    unsigned int cep;
+    char birth[11];
+} contact;
+
+typedef struct node{
+    contact data;
+    struct node *next;
+} Node;
+
+typedef struct list{
+    int size;
+    Node *head;
+} List;
+
+List* createList();
 
 int main() {
-    
+
     FILE *file; 
     file = fopen("contatos.txt", "r");
     openFile(file);
+    
+    List *lista=  createList();
+    printf("%d\n", lista -> size);
 
+    fclose(file);
     return 0;
 }
 
@@ -22,4 +47,14 @@ void openFile(FILE *file){
             printf("%s", arr);
         }
     }
+    printf("\n");
+}
+
+List* createList() {
+    List *list = (List*) malloc(sizeof(List));
+
+    list -> size = 0;
+    list -> head = NULL;
+
+    return list;
 }
