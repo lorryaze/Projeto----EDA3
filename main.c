f7833da27f5267531ccd1e2480a237b560561ca7
MarthaDs <marthadntas@gmail.com>
2018-10-19 22:23:36 -0300

    [CREATE] criando funçõs: de criar registro, inserir na lista e validação de dados do contato

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int mostrarMenu();
 void openFile(FILE *);
 
-//tipo de dado
+//contato
 typedef struct contato{
     char nome[101];
     char celular[11];
@@ -13,38 +14,43 @@ typedef struct contato{
     char data[11];
 } Contato;
 
-//elementos da lista
-typedef struct  elemento{
-	Contato dados;	
-	struct elemento *antes;
-	struct elemento *depois;
-} Elemento;
+// lista duplamente encadeada
+typedef struct lista{
+    int qntdElemetos;
+	void *contato;	
+	struct lista *anterior;
+	struct lista *prox;
+} Lista;
 
-typedef struct elemento *Lista;
-//estrutura de controle
-/*typedef struct node{
-	Elemento *inicio; //ponteiro p o primero elemento da lista
-	Elemento *fim;	//ponteiro p o ultimo elemento da lista
-	int tamanho;
-}Node; */
+// cabeçalho
+typedef struct header{
+	Lista *head; //ponteiro p o primero elemento da lista
+	Lista *tail; //ponteiro p o ultimo elemento da lista
+} Header;
 
-Lista *criaLista();
+Header *inicializaHeader();
 void liberaLista(Lista *);
+Contato *criaRegistro();
+char *validaData(char data[]);
+char *validaCelular(char celular[]);
+void insereRegistro(Header *header, Contato *contato);
+
 int main(int argc, char *argv[]) {
 
     FILE *file; 
     file = fopen("contatos.txt", "r");
     openFile(file);
     
-    Lista *li;
-    li = criaLista();
-    liberaLista(li);
+    Header *header = inicializaHeader();
+
+    //liberaLista(li);
 	        
     int opcao = 0;
     while(opcao != 5){
         opcao = mostrarMenu();
         switch(opcao){
-            case 1: //chamar função de inserir novo registro
+            case 1:
+                insereRegistro(header, criaRegistro());
                 break;
             
             case 2: //chamar função de remover registro
@@ -83,16 +89,16 @@ void openFile(FILE *file){
     printf("\n");
 }
 
-Lista *criaLista(){
-	Lista *li = (Lista*) malloc(sizeof(Lista));
-	if(li != NULL){
-		*li = NULL;
-	}
-	return li;
+Header *inicializaHeader() {
+	Header *header = (Header*) malloc(sizeof(Header));
+    header->head = NULL;
+    header->tail = NULL;
+	return header;
 }
-void liberaLista(Lista *li){
+
+/*void liberaLista(Lista *li){
 	if(li != NULL){
-		Elemento *no;
+		Lista *no;
 		while((*li) != NULL) {
 			no = *li;
 			*li = (*li) -> depois;
@@ -100,7 +106,7 @@ void liberaLista(Lista *li){
 		}
 		free(li);
 	}
-}
+} */
 
 int mostrarMenu(){
     int opcao = 0;
@@ -117,4 +123,97 @@ int mostrarMenu(){
     scanf("%d", &opcao);
 
     return opcao;
+}
+
+void insereRegistro(Header *header, Contato *contato) {
+    Lista *novoElemento = (Lista*) malloc(sizeof(Lista));
+    if(novoElemento == NULL) {
+        printf("Contato nao pode ser inserido na agenda!\n");
+    }
+        
+    novoElemento->contato = contato;
+    novoElemento->qntdElemetos++;
+
+    if(header == NULL) {
+        header->head = novoElemento;
+        header->tail = novoElemento;
+        novoElemento->prox = NULL;
+        novoElemento->anterior = NULL;
+    }
+    else {
+        novoElemento->prox = NULL;
+        novoElemento->anterior = header->tail;
+        header->tail->prox = novoElemento;
+        header->tail = novoElemento;
+        novoElemento->qntdElemetos++;
+    }
+}
+
+Contato *criaRegistro() {
+    Contato *novoContato;
+    char nome[101], celular[11];
+    char endereco[101], data[11];
+    unsigned int cep;
+
+    novoContato = (Contato*) malloc(sizeof(Contato));
+    if(novoContato == NULL) {
+        printf("Contato não criado, tente novamente...\n");
+    }
+    getchar();
+    // ler nome e armazena 
+    printf("Digite o nome: ");
+    gets(nome);
+    strcpy(novoContato->nome, nome);
+
+    // ler celular e armazena 
+    printf("\nDigte o celular(xxxxx-xxxx): ");
+    gets(celular);
+    char *correctCelular = validaCelular(celular);
+    strcpy(novoContato->celular, correctCelular);
+
+    // ler endereço e armazena 
+    printf("\nDigte o endereco: ");
+    gets(endereco);
+    strcpy(novoContato->endereco, endereco);
+
+    // ler cep e armazena 
+    printf("\nDigite o CEP: ");
+    scanf("%d", &cep);
+    novoContato->cep = cep;
+
+    getchar();
+    // ler data e armazena 
+    printf("\nDigite a data(dd/mm/aaaa): ");
+    gets(data);
+    char *correctData = validaData(data);
+    strcpy(novoContato->data, correctData);
+    
+    return novoContato;
+}
+
+char *validaData(char data[]) {
+/*     while(data[2] != '/' && data[5] != '/') {
+        printf("Valor incorreto, formato exigido: dd/mm/aaaa\n");
+        printf("\nDigte a data: ");
+        gets(data);
+    }*/
+    return data;
+}
+
+char *validaCelular(char celular[]) {
+/*     while(celular[5] != '-') {
+        printf("Valor incorreto, formato exigido: xxxxx-xxxx\n");
+        printf("\nDigte o celular: ");
+        gets(celular);
+    } */
+
+    return celular;
+}
+Lista *insertionSort(Lista *li, int tamanhoLista) {
+    int i,j,aux;
+    
+    for(j = 1; j < tamanhoLista; j++) {
+       // aux = li;
+    }
+return li;    
 }
